e5247b2b09fdf3e6e5f898726023bcfcec668e94
Maria Rabyk <mariia.rabyk@gmail.com>
2018-06-14 16:38:04 +0300

    Format sql

--- functions.sql
+++ functions.sql
@@ -1,26 +1,31 @@
 CREATE FUNCTION add(x integer, y integer) RETURNS integer AS $$
     SELECT x + y;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE PLPGSQL;
+
 
 SELECT add(6, 666) AS answer;
 
 -- SQL Functions on Composite Types
 
-CREATE TABLE emp (
-    name        text,
-    salary      numeric,
-    age         integer
- );
+CREATE TABLE emp ( name text, salary numeric, age integer );
 
-INSERT INTO emp VALUES ('Bill', 4200, 45);
+
+INSERT INTO emp
+VALUES ('Bill',
+        4200,
+        45);
+
 
 CREATE FUNCTION double_salary(emp) RETURNS numeric AS $$
     SELECT $1.salary * 2 AS salary;
 $$ LANGUAGE SQL;
 
-SELECT name, double_salary(emp.*) AS dream
-    FROM emp
-    WHERE emp.name = 'Bill';
+
+SELECT name,
+       double_salary(emp.*) AS dream
+FROM emp
+WHERE emp.name = 'Bill';
+
 
 CREATE FUNCTION new_emp() RETURNS emp AS $$
     SELECT text 'None' AS name,
@@ -28,47 +33,58 @@ CREATE FUNCTION new_emp() RETURNS emp AS $$
         25 AS age;
 $$ LANGUAGE SQL;
 
+
 CREATE OR REPLACE FUNCTION new_emp() RETURNS emp AS $$
     SELECT ROW('None', 1000.0, 25)::emp;
 $$ LANGUAGE SQL;
 
-select * from new_emp();
+
+SELECT *
+FROM new_emp();
+
+
 SELECT new_emp();
+
+
 SELECT (new_emp()).salary;
+
+
 SELECT age(new_emp());
+
 
 CREATE FUNCTION getname(emp) RETURNS text AS $$
     SELECT $1.name;
 $$ LANGUAGE SQL;
 
+
 SELECT getname(new_emp());
 
 --  SQL Functions with Output Parameters
 
-CREATE FUNCTION add_out (IN x int, IN y int, OUT sum int)
-AS $$
+CREATE FUNCTION add_out (IN x int, IN y int, OUT SUM int) AS $$
 SELECT x + y;
-$$
-LANGUAGE SQL;
-
-select * from add_out(666778, 12345678);
+$$ LANGUAGE SQL;
 
 
-CREATE FUNCTION sum_and_product (x int, y int, OUT sum int, OUT product int)
-AS $$
+SELECT *
+FROM add_out(666778, 12345678);
+
+
+CREATE FUNCTION sum_and_product (x int, y int, OUT SUM int, OUT product int) AS $$
 SELECT x + y, x * y;
-$$
-LANGUAGE SQL;
+$$ LANGUAGE SQL;
 
- SELECT * FROM sum_and_product(11111,1111);
 
-CREATE TYPE sum_prod AS (sum int, product int);
+SELECT *
+FROM sum_and_product(11111, 1111);
 
-CREATE OR REPLACE FUNCTION sum_and_product (int, int) RETURNS sum_prod
-AS $$
+
+CREATE TYPE sum_prod AS (SUM int, product int);
+
+
+CREATE OR REPLACE FUNCTION sum_and_product (int, int) RETURNS sum_prod AS $$
 SELECT $1 + $2, $1 * $2;
-$$
-LANGUAGE SQL;
+$$ LANGUAGE SQL;
 
 -- SQL Functions with Variable Numbers of Arguments
 
@@ -77,10 +93,12 @@ CREATE FUNCTION mleast(VARIADIC arr numeric[]) RETURNS numeric AS $$
 $$ LANGUAGE SQL;
 
 
-select * from generate_subscripts('{10, -1, 5, 4}'::int[], 1);
+SELECT *
+FROM generate_subscripts('{10, -1, 5, 4}'::int[], 1);
+
 
 SELECT mleast(VARIADIC ARRAY[10, -1, 5, -4.4]);
 
-SELECT mleast(VARIADIC arr := ARRAY[10, 1, 5, 4.4]);
 
+SELECT mleast(VARIADIC arr := ARRAY[10, 1, 5, 4.4]);
 
